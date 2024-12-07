923898b95ac5a972ec4a69c88a60f3393e50dbd8
Faris Osama <111440224+linzi-v7@users.noreply.github.com>
2024-12-07 16:43:01 +0200

    update .gitignore

--- sampleproject/obj/Debug/net5.0/sampleproject.AssemblyInfo.cs
+++ sampleproject/obj/Debug/net5.0/sampleproject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sampleproject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e020d414354ac2a2f3c66bd0586b1ea6315da7ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+410ce62497c04730b297e269272caa73a12126c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("sampleproject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sampleproject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
